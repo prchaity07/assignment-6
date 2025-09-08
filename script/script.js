cc953d43d31a366a70cb1e60dc39f18a1736914b
prchaity07 <prc.create@gmail.com>
2025-09-08 22:20:35 +0600

    added full design of this task

---
+++ script/script.js
@@ -0,0 +1,3 @@
+// Spinner Function
+const showSpinner = () => document.getElementById('spinner').style.display = 'block';
+const hideSpinner = () => document.getElementById('spinner').style.display = 'none';
