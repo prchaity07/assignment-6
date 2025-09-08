a9dc0af0ccd2290146bf06253cf00bb69f2625b1
prchaity07 <prc.create@gmail.com>
2025-09-09 02:20:53 +0600

    trees category loaded

--- script/script.js
+++ script/script.js
@@ -1,3 +1,39 @@
-// Spinner Function
-const showSpinner = () => document.getElementById('spinner').style.display = 'block';
-const hideSpinner = () => document.getElementById('spinner').style.display = 'none';
+// load categories
+const loadCategory = () =>{
+    fetch("https://openapi.programming-hero.com/api/categories")
+    .then(res => res.json()) 
+    .then(data => {
+       
+        displayCategory(data.categories);
+    })
+
+}
+
+const displayCategory= (allTrees) => {
+    console.log(allTrees)
+
+    const categoryContainer = document.getElementById("categories");
+    
+
+    categoryContainer.innerHTML = "";
+
+ for (let category of allTrees) {
+    console.log(category);
+
+    const categoryDiv = document.createElement("div");
+    categoryDiv.innerHTML =  `
+        <p 
+            onclick="showCart('${category.category_name}')" 
+            class="cursor-pointer m-2 px-3 py-2 rounded-lg  hover:bg-[#15803D] hover:text-white text-[#1F2937] font-semibold text-lg">
+            ${category.category_name}
+        </p>
+    `;
+
+    categoryContainer.appendChild(categoryDiv);
+}
+
+
+
+}
+
+loadCategory()
